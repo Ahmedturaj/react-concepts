2296ae954fc897d94e5c83a52ceadf15d77a04d3
Sheikhtoukir <sheikhtoukirahmedturaj013@gmail.com>
2023-03-24 22:50:17 -0700

    adding the site

---
+++ src/Components/Device/Device.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Device = (props) => {
+    return (
+        <div>
+            <h1>This is a Vite-React Project</h1>
+            <p>I have a phone , name:{props.name}</p>
+        </div>
+    );
+};
+
+export default Device;
